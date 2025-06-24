4eed0d30fe7c6135c8db07765e9b0530af2d868b
shelffim <shelffim0219@gmail.com>
2025-06-24 22:29:21 +0900

    두_수의_나눗셈

---
+++ beginner/두_수의_나눗셈.swift
@@ -0,0 +1,12 @@
+//
+//  두_수의_나눗셈.swift
+//  
+//
+//  Created by 박현준 on 6/24/25.
+//
+
+import Foundation
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    return Int(Double(num1) / Double(num2) * 1000)
+}
