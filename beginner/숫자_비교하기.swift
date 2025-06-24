1d173f06cc0f2eada48bf513b23d2dccbbad3bf1
shelffim <shelffim0219@gmail.com>
2025-06-24 22:15:29 +0900

    숫자_비교하기

---
+++ beginner/숫자_비교하기.swift
@@ -0,0 +1,12 @@
+//
+//  숫자_비교하기.swift
+//  
+//
+//  Created by 박현준 on 6/24/25.
+//
+
+import Foundation
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    return num1 == num2 ? 1 : -1
+}
